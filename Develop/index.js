ba97b4a94834aabeff5cfeb5cab834b570489a00
Tyson Trotz <slavatrotz@gmail.com>
2023-08-25 21:59:48 -0500

    added function to create ReadMe, added 1st 2 questions, linked (requires)

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { type: "input", name: "title", message: "Enter your project title." },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project.",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("file created");
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+  const template = generateMarkdown(answers);
+  console.log(template);
+  writeToFile("README.md", template);
+});
